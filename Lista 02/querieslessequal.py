2e8e6316bc5b6b649f2d17924a59a12828eac543
lucasemmanuelsa <lucasemmanuelsa@gmail.com>
2023-03-27 18:27:13 -0300

    Queries

--- Lista 02/querieslessequal.py
+++ Lista 02/querieslessequal.py
@@ -6,14 +6,42 @@ listaA = []
 for i in a:
     listaA.append(int(i))
 
+listaA = sorted(listaA)
 listaB = []
 
 for j in b:
     listaB.append(int(j))
 
-inicio = 0
-fim = len(listaA) - 1
-meio = (fim - inicio + 1)//2
 
-while(meio < len()):
-#pega o meio, olha pra o elemento da frente e olha pro elemente de tras
+def binarySearch(inicio, final, lista, element):
+    meio = inicio + (final - inicio)// 2
+    if(inicio > final):
+        return meio
+    if(element == listaA[meio]):
+        return meio
+    if(element < lista[meio]):
+        if(element < lista[meio - 1]):
+            return binarySearch(inicio, meio - 1, lista, element)
+        else:
+            return meio - 1
+    else:
+        return binarySearch(meio + 1, final, lista, element)
+
+def binarySearchIterativo(inicio, final, lista, element):
+    while(inicio <= final):
+        meio = inicio + (final - inicio)//2
+        if(element == lista[meio]):
+            return meio
+        elif(element < lista[meio]):
+            if(element < lista[meio - 1]):
+                return meio -1
+            final = meio - 1
+        else:
+            inicio = meio + 1
+    return meio
+
+for i in listaB:
+    print(binarySearchIterativo(0, len(listaA) - 1, listaA, i) + 1, end=" ")
+'''
+for i in listaB:
+    print(binarySearch(0, len(listaA) - 1, listaA, i) + 1, end=" ")'''
